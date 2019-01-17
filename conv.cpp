850b1068d8618d1020bde1ea8e26ab506ca270bd
Harshit Jain <ee1160443@ee.iitd.ac.in>
2019-01-17 18:39:57 +0530

    README updated

--- conv.cpp
+++ conv.cpp
@@ -3,87 +3,87 @@
 #include <string>
 #include "helper.h"
 using namespace std;
-int n,f,p;
+
+int n, f, p;
+
 float** computeConv0(float** inputMatrix, float** squareKernel) {
-	//cout<<"checkpoint3"<<endl;
-	int d=(n+2*p-f + 1);
+	int d = (n + 2*p - f + 1);
 	float** conv;
-	conv=new float*[d];
-	for(int i=0;i<d;i++)
-		conv[i]=new float[d];
-	for (int i = 0; i < n+2*p-f + 1; i++) {
-		for (int j = 0; j < n+2*p-f + 1; j++) {
+	conv = new float*[d];
+	for (int i = 0; i < d; i++)
+		conv[i] = new float[d];
+	for (int i = 0; i < n + 2*p - f + 1; i++) {
+		for (int j = 0; j < n + 2*p - f + 1; j++) {
 			conv[i][j] = 0;
-			for (int u = i; u < i+f; u++) {
-				for (int v = j; v < j+f; v++) {
-					//cout<<inputMatrix[u][v]<<" "<<squareKernel[i+f-1-u][j+f-1-v]<<endl;
-					conv[i][j] += inputMatrix[u][v] * squareKernel[i+f-1-u][j+f-1-v];
-					//cout<<conv[i][j]<<endl;
+			for (int u = i; u < i + f; u++) {
+				for (int v = j; v < j + f; v++) {
+					conv[i][j] += inputMatrix[u][v] * squareKernel[i + f - 1 - u][j + f - 1 - v];
 				}
 			}
-			//cout<<conv[i][j]<<endl;
 		}
-	}
-	//cout<<"checkpoint4"<<endl;
-	return conv;
-}
-float** computeConv1(float** inputMatrix, float** squareKernel) {
-	//cout<<"checkpoint3"<<endl;
-	float teoplitz[(n+2*p-f+1)*(n+2*p-f+1)][f*f];
-	//cout<<"checkpoint4"<<endl;
-	for(int i=0;i<(n+2*p-f+1)*(n+2*p-f+1);i++)
-	{
-		int r=(i)/(n+2*p-f+1);
-		int c=i%(n+2*p-f+1);
-		for(int j=0;j<f*f;j++)
-		{
-			int r1=r+j/f;
-			int c1=c+j%f;
-			//cout<<"checkpoint"<<endl;
-			teoplitz[i][j]=inputMatrix[r1][c1];
-		}
-	}
-	//cout<<"checkpoint5"<<endl;
-	float flipedkernal[f*f];
-	int temp=0;
-	for(int i=f-1;i>=0;i--)
-	{
-		for(int j=f-1;j>=0;j--)
-		{
-			flipedkernal[temp]=squareKernel[i][j];
-			temp++;
-		}
-	}
-	//cout<<"checkpoint6"<<endl;
-	int d=(n+2*p-f + 1);
-	float** conv;
-	conv=new float*[d];
-	for(int i=0;i<d;i++)
-		conv[i]=new float[d];
-	//cout<<"checkpoint7"<<endl;
-	//float conv[n+2*p-f + 1][n+2*p-f + 1];
-	for(int i=0;i<(n+2*p-f+1)*(n+2*p-f+1);i++)
-	{
-		int sum=0;
-		for(int j=0;j<f*f;j++)
-		{
-			sum+=teoplitz[i][j]*flipedkernal[j];
-		}
-		int r=i/(n+2*p-f+1);
-		int c=i%(n+2*p-f+1);
-		conv[r][c]=sum;
 	}
 	return conv;
 }
 
-int main(int argc,char** argv){
-	p=atoi(argv[2]);
-	n=stoi(argv[4]);
-	f=stoi(argv[6]);
-	char* m1=argv[3];
-	char* m2=argv[5];
-	int mode=stoi(argv[1]);
-	int n1=n+2*p;
+float** computeConv1(float** inputMatrix, float** squareKernel) {
+	float teoplitz[(n + 2*p - f + 1)*(n + 2*p - f + 1)][f*f];
+	for(int i = 0; i < (n + 2*p - f + 1)*(n + 2*p - f + 1); i++)
+	{
+		int r = (i)/(n + 2*p - f + 1);
+		int c = i % (n + 2*p - f + 1);
+		for(int j = 0; j < f*f; j++)
+		{
+			int r1 = r + j/f;
+			int c1 = c + j%f;
+			//cout<<"checkpoint"<<endl;
+			teoplitz[i][j] = inputMatrix[r1][c1];
+		}
+	}
+
+	float flippedkernal[f*f];
+	int temp = 0;
+	for(int i = f - 1; i >= 0; i--)
+	{
+		for(int j = f - 1; j >= 0; j--)
+		{
+			flippedkernal[temp] = squareKernel[i][j];
+			temp++;
+		}
+	}
+
+	int d = (n + 2*p - f + 1);
+	float** conv;
+	conv = new float*[d];
+	for(int i = 0; i < d; i++)
+		conv[i] = new float[d];
+
+	for(int i = 0; i < (n + 2*p - f + 1)*(n + 2*p - f + 1); i++)
+	{
+		int sum = 0;
+		for(int j = 0; j < f*f; j++)
+		{
+			sum += teoplitz[i][j] * flipedkernal[j];
+		}
+		int r = i/(n + 2*p - f + 1);
+		int c = i%(n + 2*p - f + 1);
+		conv[r][c] = sum;
+	}
+	return conv;
+}
+
+int main(int argc, char** argv){
+	if (argc != 7) {
+		cerr << "Please use the correct input format: ./conv.o mode padding file_1 matrix_dimension file_2 kernel_dimension";
+		exit(1);
+	}
+
+	p = atoi(argv[2]);
+	n = stoi(argv[4]);
+	f = stoi(argv[6]);
+	char* m1 = argv[3];
+	char* m2 = argv[5];
+	int mode = stoi(argv[1]);
+	int n1 = n + 2*p;
 	//convolution_formula=0  and as matrix multiplication=1
 	float **inputMatrix;
 	inputMatrix = new float *[n1];
