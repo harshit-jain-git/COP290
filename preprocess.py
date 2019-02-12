04a418600c1ff6778fad2617b99cbaaeb9262826
Harshit Jain <harshitjain1371999@gmail.com>
2019-02-13 02:30:57 +0530

    code updated

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,9 @@
 import cv2
-import numpy as np;
-img = cv2.imread('./1/1.png',0)
+import numpy as np
+import sys
+
+path = sys.argv[1]
+img = cv2.imread(path,0)
 if img.shape != [28,28]:
     img2 = cv2.resize(img,(28,28))
     img = img2.reshape(28,28,-1);
@@ -8,3 +11,9 @@ else:
     img = img.reshape(28,28,-1);
 #revert the image,and normalize it to 0-1 range
 img = 1.0 - img/255.0
+
+file = open("data.txt", 'w')
+
+for i in range(0, 28):
+    for j in range(0, 28):
+        file.write(str(img[i][j][0]) + "\n")
